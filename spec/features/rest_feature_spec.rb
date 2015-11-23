8a88ed21c65fd1196b7db48de160266da189c180
samover <samuel.overloop@gmail.com>
2015-11-23 14:53:41 +0000

    add restaurant model

--- spec/features/rest_feature_spec.rb
+++ spec/features/rest_feature_spec.rb
@@ -8,4 +8,15 @@ feature 'Restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
+
+  context 'Restaurants have been added' do
+    before do
+      Restaurant.create( name: 'Bocado' )
+    end
+    scenario 'Display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content 'Bocado'
+      expect(page).not_to have_content 'No restaurants yet'
+    end
+  end
 end
