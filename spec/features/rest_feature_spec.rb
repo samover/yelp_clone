f330626f501e422955d11f4f904e22b6a5161398
samover <samuel.overloop@gmail.com>
2015-11-23 15:53:58 +0000

    create new restaurant: controller and view

--- spec/features/rest_feature_spec.rb
+++ spec/features/rest_feature_spec.rb
@@ -10,13 +10,27 @@ feature 'Restaurants' do
   end
 
   context 'Restaurants have been added' do
+
     before do
       Restaurant.create( name: 'Bocado' )
     end
+
     scenario 'Display restaurants' do
       visit '/restaurants'
       expect(page).to have_content 'Bocado'
       expect(page).not_to have_content 'No restaurants yet'
+    end
+
+  end
+
+  context 'Creating restaurants' do
+    scenario 'Prompts user to fill out a form, then displays new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Bocado'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Bocado'
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
